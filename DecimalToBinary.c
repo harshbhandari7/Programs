d8bd95d343d7032ec4776e8aaf30c74a93443358
FireFeathers06 <harshit14feb@gmail.com>
2019-10-07 00:02:38 +0530

    Create DecimalToBinary.c
    Decimal To Binary using right-shift bit-wise operator

---
+++ DecimalToBinary.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void toBinary(int n) {
+    /*
+    :type a: int
+    
+    bits: Number of characters to display Eg. 8 bits = 0000000 Which would get filled up till 2^8-1
+    >>: Using Right Shift Bitwise operator to get each bit on units place and comparing it with True
+    
+    Example 1: 
+    Input: 2
+    Output: 00000010
+    
+    Example 2:
+    Input: 100
+    Output: 01100100
+    */
+    
+    int bits = 8; // 
+        for(int i = bits; i >= 0; i--){
+            if ((n>>i & 1)== 1) printf("1"); // 
+            else printf("0");
+        }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    toBinary(n);
+}
